f4ac9e6c1958664cbcdd119ba2bf6a6e41aea372
Mateus Alves <contatomateusalves@hotmail.com>
2025-02-07 13:52:01 -0300

    feat: teste de processamento

---
+++ 02-teste-processamento/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int x, y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println("Área = " + area);
+
+        int a, c;
+        double resultado;
+
+        a = 5;
+        c = 2;
+
+        // casting
+        resultado = (double) a / c;
+
+        System.out.println(resultado);
+    }
+}
