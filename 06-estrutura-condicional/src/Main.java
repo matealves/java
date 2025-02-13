8fde60e5134c6d4a983bc692df050d98de801fa2
Mateus Alves <contatomateusalves@hotmail.com>
2025-02-13 19:19:08 -0300

    feat: estrutura condicional

---
+++ 06-estrutura-condicional/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int x = 5;
+
+        if (x > 0 && x < 12) {
+            System.out.println("Bom dia!");
+        } else if (x >= 12 && x < 18) {
+            System.out.println("Boa tarde!");
+        } else {
+            System.out.println("Boa noite");
+        }
+
+    }
+}
