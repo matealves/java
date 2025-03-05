8784f320ece241b44d0a77b7687ca7b8f8d40d48
Mateus Alves <contatomateusalves@hotmail.com>
2025-03-05 19:48:40 -0300

    feat: condicao ternaria

---
+++ 10-condicao-ternaria/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
+
+    }
+}
