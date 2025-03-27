56606fd2e9f6d4444620f4fe9aad4e0d5cbf2344
Mateus Alves <contatomateusalves@hotmail.com>
2025-03-27 20:32:31 -0300

    feat: metodo area

--- OO-introducao/src/entities/Triangle.java
+++ OO-introducao/src/entities/Triangle.java
@@ -5,4 +5,9 @@ public class Triangle {
     public double a;
     public double b;
     public double c;
+
+    public double area() {
+        double s = (a + b + c) / 2.0;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
 }
